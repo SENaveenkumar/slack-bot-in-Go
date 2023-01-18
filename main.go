55004f533a127c268bad9a27233f028f84c2433b
Naveenkumar S E <98598772+SENaveenkumar@users.noreply.github.com>
2023-01-19 00:04:10 +0530

    File-upload-bot

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4276721590384-4411304353552-6Oaz3xVfqejhjg6OsGZmAQC3")
+	os.Setenv("CHANNEL_ID", "C04BE87NZ3L")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"myresume.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s \n", file.Name, file.URL)
+	}
+}
